441cf5a7bca881ae3fe9c8c9aadfcff1838fa5d0
Byron Lee Jr <leebyron120@gmail.com>
2023-07-26 16:57:30 -0400

    day 1 end card w/ li

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 "use strict";
+
+const cardContainer = document.querySelector("#card-container");
+const card = document.querySelector(".card");
+for (let i = 0; i < 12; i++) {
+  const card = document.createElement("li");
+  card.classList.add("card", "back");
+  cardContainer.append(card);
+}
+
+cardContainer.addEventListener("click", (e) => {
+  if (e.target.classList.contains("card")) {
+    e.target.classList.toggle("flipCard");
+    e.target.classList.toggle("front");
+    console.log("I touched the butt");
+  }
+});
