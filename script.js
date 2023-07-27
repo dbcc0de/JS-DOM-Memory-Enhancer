b2240a0b4888df53321a7a8e6dde779aece1c6e7
Ryan Hall <dillbc@protonmail.com>
2023-07-27 10:02:37 -0400

    achieved first click flip, need second click flip

--- script.js
+++ script.js
@@ -4,14 +4,24 @@ const cardContainer = document.querySelector("#card-container");
 const card = document.querySelector(".card");
 for (let i = 0; i < 12; i++) {
   const card = document.createElement("li");
-  card.classList.add("card", "back");
+  card.classList.add("card");
+  const cardFlipInner = document.createElement("div");
+  cardFlipInner.classList.add("flipCard");
+  const cardFront = document.createElement("div");
+  cardFront.classList.add("front");
+  const cardBack = document.createElement("div");
+  cardBack.classList.add("back");
+  cardFlipInner.append(cardFront);
+  cardFlipInner.append(cardBack);
+  card.append(cardFlipInner);
   cardContainer.append(card);
+  card.classList.add("back");
 }
 
 cardContainer.addEventListener("click", (e) => {
   if (e.target.classList.contains("card")) {
     e.target.classList.toggle("flipCard");
     e.target.classList.toggle("front");
-    console.log("I touched the butt");
+    e.target.classList.toggle("back");
   }
 });
